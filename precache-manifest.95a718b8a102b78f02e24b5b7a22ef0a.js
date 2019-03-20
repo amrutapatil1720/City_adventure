0116701bfd376adbcdd001914afb971ff55e50bb
Amruta <amrutapatil1720@gmail.com>
2019-03-19 17:51:36 -0700

    Updates

---
+++ precache-manifest.95a718b8a102b78f02e24b5b7a22ef0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f6cf80ffb5428b3195b",
+    "url": "/City_adventure/static/js/runtime~main.4f6cf80f.js"
+  },
+  {
+    "revision": "215a23bcf544f188ecdc",
+    "url": "/City_adventure/static/js/main.215a23bc.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/City_adventure/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "215a23bcf544f188ecdc",
+    "url": "/City_adventure/static/css/main.17ea79af.chunk.css"
+  },
+  {
+    "revision": "21a8a0a7c4be43bfb0efbbb26bdd07ea",
+    "url": "/City_adventure/index.html"
+  }
+];
